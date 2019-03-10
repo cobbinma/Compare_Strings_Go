32e6dad4a4b30f194f88e4f43263c38860fd8681
cobbinma <cobbinma@gmail.com>
2019-03-10 22:04:09 +0000

    added byte arrays for text and subtext

--- comparison.go
+++ comparison.go
@@ -3,12 +3,20 @@ package main
 import "fmt"
 
 type comparison struct {
-	text    string
-	subtext string
+	text             string
+	subtext          string
+	textByteArray    []byte
+	subtextByteArray []byte
 }
 
-func setComparison(text string, subtext string) comparison {
-	comp := comparison{text, subtext}
+func createByteArray(givenString string) []byte {
+	byteArray := []byte(givenString)
+	return byteArray
+}
+
+func setComparison(text string, subtext string, textByteArray []byte,
+	subtextByteArray []byte) comparison {
+	comp := comparison{text, subtext, textByteArray, subtextByteArray}
 	return comp
 }
 
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 func main() {
 	args := os.Args[1:]
-	comp1 := comparison{args[0], args[1]}
-	comp1.printComparison()
+	textArray := createByteArray(args[0])
+	subTextArray := createByteArray(args[1])
+	comp1 := comparison{args[0], args[1], textArray, subTextArray}
+	fmt.Println(comp1)
 }
