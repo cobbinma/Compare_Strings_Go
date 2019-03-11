903be5619b758cc44c335dbdacf5a2b117dc5edf
cobbinma <cobbinma@gmail.com>
2019-03-11 22:23:59 +0000

    add unit test

--- comparison_test.go
+++ comparison_test.go
@@ -15,3 +15,15 @@ func TestComparisonSetComparison(t *testing.T) {
 		t.Fatalf("Expected %s but got %s", subtext, comparisonTest.subtext)
 	}
 }
+
+func TestCompareByteArrays(t *testing.T) {
+	var text = "hsadkjahsdkjateghgfhgfhfsttestafasfasf"
+	var subtext = "test"
+	var expectedMatch = 27
+	comparisonTest := setComparison(text, subtext, createByteArray(text), createByteArray(subtext))
+	matchArray := comparisonTest.compareByteArrays()
+
+	if matchArray[0] != expectedMatch {
+		t.Fatalf("Expected match at %v but did there was not", expectedMatch)
+	}
+}
